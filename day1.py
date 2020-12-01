3271961f6a737ac7f1ae66b7918d7474616c235d
Daniel Harris <daniel_harris@daniels-mbp.mynet>
2020-12-01 07:01:39 +0000

    day 1

---
+++ day1.py
@@ -0,0 +1,14 @@
+"""Report Repair"""
+
+with open('inputs/day1.txt', 'r') as f:
+    inp = [int(x) for x in f.read().split()]
+
+num_inp, target = len(inp), 2020
+for ip1 in range(num_inp):
+    found = False
+    for ip2 in range(ip1+1, num_inp):
+        if inp[ip1] + inp[ip2] == target:
+            print(f'answer to puzzle 1 is {inp[ip1] * inp[ip2]}')
+        for ip3 in range(ip2+1, num_inp):
+            if inp[ip1] + inp[ip2] + inp[ip3] == target:
+                print(f'answer to puzzle 2 is {inp[ip1] * inp[ip2] * inp[ip3]}')
