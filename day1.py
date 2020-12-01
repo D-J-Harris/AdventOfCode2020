6f9bdc76f41c1b7cb15c09fad64b73cdca6fc649
Daniel Harris <daniel_harris@daniels-mbp.mynet>
2020-12-01 07:07:09 +0000

    day 1

--- day1.py
+++ day1.py
@@ -1,4 +1,6 @@
 """Report Repair"""
+from itertools import combinations
+from math import prod
 
 with open('inputs/day1.txt', 'r') as f:
     inp = [int(x) for x in f.read().split()]
@@ -12,3 +14,7 @@ for ip1 in range(num_inp):
         for ip3 in range(ip2+1, num_inp):
             if inp[ip1] + inp[ip2] + inp[ip3] == target:
                 print(f'answer to puzzle 2 is {inp[ip1] * inp[ip2] * inp[ip3]}')
+
+# solution I wish I'd thought of
+print([prod(c) for c in combinations(inp, 2) if sum(c) == 2020])
+print([prod(c) for c in combinations(inp, 3) if sum(c) == 2020])
