2ab171c91f22215eeb3b169e8a10e4e139f31450
Daniel Harris <daniel_harris@192.168.1.104>
2020-12-05 11:38:45 +0000

    improve generalisation

--- days/day05.py
+++ days/day05.py
@@ -8,19 +8,18 @@ with open('../inputs/day05.txt', 'r') as f:
 
 def seat_id(board_pass):
     row, col = 0, 0
-    for j in range(7):
+    row_instr, col_instr = 7, 3
+    for j in range(row_instr + col_instr):
         zone = board_pass[j]
         if zone == 'B':
-            row += 2 ** (6-j)
-    for k in range(3):
-        zone = board_pass[k + 7]
+            row += 2 ** ((row_instr-1)-j)
         if zone == 'R':
-            col += 2 ** (2-k)
+            col += 2 ** ((col_instr-1)-(j-row_instr))
     return 8 * row + col
 
 
 seat_ids = sorted([seat_id(line) for line in inp])
 print(f'answer to puzzle 1 is {max(seat_ids)}')
 for i in range(1, len(seat_ids)):
-    if seat_ids[i] - seat_ids[i-1] - 1 != 0:
+    if seat_ids[i] - seat_ids[i-1] != 1:
         print(f'answer to puzzle 2 is {seat_ids[i]-1}')
