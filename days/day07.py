247a5fe411f3e3c2ac90996b274a42c4c488bf9c
Daniel Harris <daniel_harris@daniels-mbp.mynet>
2020-12-07 10:43:47 +0000

    day 7

---
+++ days/day07.py
@@ -0,0 +1,36 @@
+"""Handy Haversacks"""
+from collections import defaultdict
+import networkx as nx
+
+graph = nx.DiGraph()
+bags = defaultdict(lambda: len(bags))
+with open('../inputs/day07.txt', 'r') as f:
+    for line in f.readlines():
+        inp = line.replace(',', '').rstrip().split(' ')
+        num_children = (len(inp) - 4) // 4
+        parent_colour = '-'.join(inp[:2])
+        for i in range(num_children):
+            colour = '-'.join(inp[5+4*i:7+4*i])
+            weight = inp[4+4*i]
+            graph.add_edge(bags[parent_colour], bags[colour], weight=weight)
+
+
+def children_freqs(g, node):
+    return [(n, int(g[node][n]["weight"])) for n in nx.neighbors(g, node)]
+
+
+def bag_total(g, node):
+    total = 0
+    cfs = children_freqs(g, node)
+    for pair in cfs:
+        child, freq = pair
+        child_children = children_freqs(graph, child)
+        if child_children:
+            total += freq * (1 + bag_total(g, child))
+        else:
+            total += freq
+    return total
+
+
+print(f'answer to puzzle 1 is {len(nx.ancestors(graph, bags["shiny-gold"]))}')
+print(f'answer to puzzle 2 is {bag_total(graph, bags["shiny-gold"])}')
