21fe89383225dddaebdd1633b1ec538e7d6a5bca
Daniel Harris <daniel_harris@daniels-mbp.mynet>
2020-12-11 11:48:26 +0000

    cleaned day 11

--- days/day11.py
+++ days/day11.py
@@ -10,22 +10,13 @@ adj = [(i, j) for i in (-1, 0, 1) for j in (-1, 0, 1) if not (i == j == 0)]
 max_x, max_y = len(inp[0]), len(inp)
 
 
-def get_adjacent_cells(g, x, y):
-    result = []
-    for pair in adj:
-        dx, dy = pair
-        if 0 <= (x + dx) < max_x and 0 <= y + dy < max_y:
-            result.append(g[y + dy][x + dx])
-    return result
-
-
-def count_visible_cells(g, x, y):
+def count_visible(g, x, y, first_part):
     result = [0] * 8
     for idx, pair in enumerate(adj):
-        dx_orig, dy_orig = pair
-        for mult in range(1, max(x, y, max_x-1-x, max_y-1-y)):
-            dy = dy_orig * mult
-            dx = dx_orig * mult
+        scan_range = max(x, y, max_x-x-1, max_y-y-1)
+        for mult in range(1, (2 if first_part else scan_range)):
+            coords = tuple(mult * x for x in pair)
+            dx, dy = coords
             if 0 <= (x + dx) < max_x and 0 <= y + dy < max_y:
                 seat =g[y + dy][x + dx]
                 if seat == '#':
@@ -33,53 +24,32 @@ def count_visible_cells(g, x, y):
                     break
                 if seat == 'L':
                     break
-    return result
+    return sum(result)
 
 
-def one_pass(g):
-    grid = deepcopy(g)
+def one_pass(g, first_part):
+    updates = []
     for y in range(max_y):
         for x in range(max_x):
-            num_occupied = get_adjacent_cells(g, x, y).count('#')
+            num_occupied = count_visible(g, x, y, first_part=first_part)
             if g[y][x] == 'L' and num_occupied == 0:
-                grid[y][x] = '#'
-            if g[y][x] == '#' and num_occupied > 3:
-                grid[y][x] = 'L'
-    return grid
+                updates.append([x, y, '#'])
+            if g[y][x] == '#' and num_occupied > (3 if first_part else 4):
+                updates.append([x, y, 'L'])
 
-grid = deepcopy(inp)
-while True:
-    new_grid = one_pass(grid)
-    #for i in new_grid:
-    #    print(i)
-    #print("\n")
-    if new_grid == grid:
-        print(f"answer to puzzle 1 is {sum([x.count('#') for x in new_grid])}")
-        break
-    grid = new_grid
+    for update in updates:
+        x, y, val = update
+        g[y][x] = val
+    return True if updates else False
 
-def one_pass_new(g):
-    grid = deepcopy(g)
-    for y in range(max_y):
-        for x in range(max_x):
-            a = count_visible_cells(g, x, y)
-            num_occupied = sum(a)
-            if g[y][x] == 'L' and num_occupied == 0:
-                grid[y][x] = '#'
-            if g[y][x] == '#' and num_occupied > 4:
-                grid[y][x] = 'L'
-    return grid
 
-grid = deepcopy(inp)
-while True:
-    new_grid = one_pass_new(grid)
-    #print(adj)
-    #for i in new_grid:
-    #    print(i)
-    #print("\n")
-    if new_grid == grid:
-        print(f"answer to puzzle 2 is {sum([x.count('#') for x in new_grid])}")
-        break
-    grid = new_grid
+def complete_game(first_part):
+    grid = deepcopy(inp)
+    while True:
+        changed = one_pass(grid, first_part=first_part)
+        if not changed:
+            return sum([x.count('#') for x in grid])
 
-#print(f'answer to puzzle 2 is {np.prod([trees_for_trajectory(inp, t) for t in p2_trajectories])}')
+
+print(f'answer to puzzle 1 is {complete_game(first_part=True)}')
+print(f'answer to puzzle 2 is {complete_game(first_part=False)}')
