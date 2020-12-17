d89e8639e8975b8835431bbc0f1c83f05694043b
Daniel Harris <daniel_harris@daniels-mbp.mynet>
2020-12-17 07:36:00 +0000

    day 17

--- days/day17.py
+++ days/day17.py
@@ -1,11 +1,9 @@
 """Conway Cubes"""
 from copy import deepcopy
 import numpy as np
-import numpy
-import sys
-numpy.set_printoptions(threshold=sys.maxsize)
-
 num_cycles = 6
+
+
 def pad_with(vector, pad_width, iaxis, kwargs):
     pad_value = kwargs.get('padder', num_cycles)
     vector[:pad_width[0]] = pad_value
@@ -17,52 +15,50 @@ with open('../inputs/day17.txt', 'r') as f:
     for line in f.read().splitlines():
         inp.append([c for c in line])
     inp = np.array(inp)
-max_x, max_y, max_z = len(inp[0]) + 2*num_cycles, len(inp) + 2*num_cycles, 1 + 2*num_cycles
+max_x, max_y, max_z, max_w = len(inp[0]) + 2*num_cycles, len(inp) + 2*num_cycles, 1 + 2*num_cycles, 1 + 2*num_cycles
 
-cube = np.array([[['.' for row in range(max_x)] for col in range(max_y)] for layer in range(max_z)])
-cube[num_cycles] = np.pad(inp, 6, pad_with, padder='.')
-#print(cube)
+cube = np.array([[[['.' for row in range(max_x)] for col in range(max_y)] for layer in range(max_z)] for hyp in range(max_w)])
+cube[num_cycles][num_cycles] = np.pad(inp, 6, pad_with, padder='.')
 
 
-def count_active(c, x_coord, y_coord, z_coord, first_part):
+def count_active(c, x_coord, y_coord, z_coord, w_coord):
     result = {}
     for dx in (-1, 0, 1):
         for dy in (-1, 0, 1):
             for dz in (-1, 0, 1):
-                if dx == 0 and dy == 0 and dz == 0:
-                    continue
-                x, y, z = x_coord + dx, y_coord + dy, z_coord + dz
-                if 0 <= x < max_x and 0 <= y < max_y and 0 <= z < max_z:
-                    cube = c[z][y][x]
-                    if cube == '#':
-                        result[(dx, dy, dz)] = 1
+                for dw in (-1, 0, 1):
+                    if dx == 0 and dy == 0 and dz == 0 and dw == 0:
+                        continue
+                    x, y, z, w = x_coord + dx, y_coord + dy, z_coord + dz, w_coord + dw
+                    if 0 <= x < max_x and 0 <= y < max_y and 0 <= z < max_z and 0 <= w < max_w:
+                        cube = c[w][z][y][x]
+                        if cube == '#':
+                            result[(dx, dy, dz, dw)] = 1
     return sum(result.values())
 
 
-def one_pass(c, first_part):
+def one_pass(c):
     updates = []
-    for z in range(max_z):
-        for y in range(max_y):
-            for x in range(max_x):
-                num_active = count_active(c, x, y, z, first_part=first_part)
-                if c[z][y][x] == '#' and (num_active < 2 or num_active > 3):
-                    updates.append([x, y, z, '.'])
-                if c[z][y][x] == '.' and num_active == 3:
-                    updates.append([x, y, z, '#'])
+    for w in range(max_w):
+        for z in range(max_z):
+            for y in range(max_y):
+                for x in range(max_x):
+                    num_active = count_active(c, x, y, z, w)
+                    if c[w][z][y][x] == '#' and (num_active < 2 or num_active > 3):
+                        updates.append([x, y, z, w, '.'])
+                    if c[w][z][y][x] == '.' and num_active == 3:
+                        updates.append([x, y, z, w, '#'])
     for update in updates:
-        x, y, z, val = update
-        c[z][y][x] = val
+        x, y, z, w, val = update
+        c[w][z][y][x] = val
     return True if updates else False
 
 
-def complete_game(first_part):
+def complete_game():
     game_cube = deepcopy(cube)
     for i in range(num_cycles):
-        one_pass(game_cube, first_part=first_part)
-        #print(f'======= cycle {i} ========')
-        #print(game_cube)
+        one_pass(game_cube)
     print(np.count_nonzero(game_cube == '#'))
 
-complete_game(True)
-#print(f'answer to puzzle 1 is {complete_game(first_part=True)}')
-#print(f'answer to puzzle 2 is {complete_game(first_part=False)}')
+
+print(f'answer to puzzle 2 is {complete_game()}')
